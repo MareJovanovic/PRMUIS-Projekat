5d47b984104e20bd2d76a38dac97578751fa40ff
Balsa <balsamilovic2004@gmail.com>
2026-02-08 19:20:23 +0100

    Dodat servis za dodavanje knjige

--- PRIMUS-Projekat/PRIMUS-Projekat/MainWindow.xaml.cs
+++ PRIMUS-Projekat/PRIMUS-Projekat/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using PRIMUS_Projekat.Src;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,10 +18,62 @@ namespace PRIMUS_Projekat
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    { 
+        private KnjigaServis knjigaServis;
+        new ObservableCollection<Knjiga> knjige;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            knjigaServis = new KnjigaServis();
+            knjige = new ObservableCollection<Knjiga>(knjigaServis.getKnjige());
+
+            PrikazKnjiga.ItemsSource = knjige;
+            BookLIst.ItemsSource = knjige;
+        }
+       
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddBook_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Knjiga k = new Knjiga(NaslovK.Text, AutorK.Text, int.Parse(KolicinaK.Text));
+                if (knjigaServis.Dodaj(k))
+                {
+                    knjige.Add(k);
+                    MessageBox.Show("Uspesno ste dodali knjigu");
+                    NaslovK.Clear();
+                    AutorK.Clear();
+                    KolicinaK.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Greska pri unosu");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Neispravan format unosa");
+            }
+        }
+
+        private void DLTBook_Click(object sender, RoutedEventArgs e)
+        {
+            if (System.Windows.MessageBox.Show("Da li ste sigurni da zelite da obrisete knjigu", "Potvrdi brisanje", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                knjige.RemoveAt(PrikazKnjiga.SelectedIndex);
+                MessageBox.Show("Knjiga obrisana");
+            }
+            else
+            {
+                MessageBox.Show("Neuspesno brisanje");
+            }
         }
     }
 }
---
+++ PRIMUS-Projekat/PRIMUS-Projekat/Src/KnjigaServis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIMUS_Projekat.Src
+{
+    class KnjigaServis
+    {
+        private List<Knjiga> knjige = new List<Knjiga> { };
+        
+        public KnjigaServis()
+        {
+            knjige.Add(new Knjiga("Na Drini ćuprija", "Ivo Andrić", 10));
+            knjige.Add(new Knjiga("Zov daljine", "Ljubica Arsić", 20));
+            knjige.Add(new Knjiga("Lovac u žitu", "Džerom Dejvid Selindžer", 5));
+        }
+
+        public List<Knjiga> getKnjige()
+        {
+            return knjige;
+        }
+        public bool Dodaj(Knjiga k)
+        {
+            if (k == null || k.Naslov == "" || k.Autor == "" || k.Kolicina == 0)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (Knjiga knj in knjige)
+                {
+                    if (knj.Naslov == k.Naslov && knj.Autor == k.Autor && knj.Kolicina == k.Kolicina)
+                    {
+                        return false;
+                    }
+                }
+                knjige.Add(k);
+                return true;
+            }
+        }
+    }
+}
