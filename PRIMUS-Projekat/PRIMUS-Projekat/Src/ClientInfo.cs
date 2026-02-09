54acc7be3a4711b901e5aa66efe9918fa43d2c82
Balsa <balsamilovic2004@gmail.com>
2026-02-09 01:47:45 +0100

    Napravlejn dijagram zadatka

---
+++ PRIMUS-Projekat/PRIMUS-Projekat/Src/ClientInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIMUS_Projekat.Src
+{
+    class ClientInfo
+    {
+            public int Id { get; set; }
+            public TcpClient Client { get; set; }
+            public NetworkStream Stream { get; set; }
+    }
+}
