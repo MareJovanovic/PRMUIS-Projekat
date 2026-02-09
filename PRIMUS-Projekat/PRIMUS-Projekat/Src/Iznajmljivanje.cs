687a618c679f37875397a07b3b5013ee6199ef93
Marko Jovanovic <jovmarko04@gmail.com>
2026-02-09 11:38:26 +0100

    Uradjen zadatak 4

---
+++ PRIMUS-Projekat/PRIMUS-Projekat/Src/Iznajmljivanje.cs
@@ -0,0 +1,20 @@
+﻿namespace PRIMUS_Projekat.Src
+{
+    public class Iznajmljivanje
+    {
+        public string Naslov { get; set; }
+        public string Autor { get; set; }
+        public int ClanId { get; set; }
+        public DateTime DatumVracanja { get; set; }
+        public int BrojPrimeraka { get; set; }
+
+        public Iznajmljivanje(string naslov, string autor, int clanId, int broj)
+        {
+            Naslov = naslov;
+            Autor = autor;
+            ClanId = clanId;
+            BrojPrimeraka = broj;
+            DatumVracanja = DateTime.Now.AddDays(14);
+        }
+    }
+}
